d36eba3407c1984037feaa3b5104ba7506631098
Bhushan Songire <bhushanbsongire@gmail.com>
2024-04-30 16:08:53 +0530

    restructuring files

--- temperature.js
+++ temperature.js
@@ -1,45 +1,45 @@
 function plot3(bs_temp) {
-// Data for the chart
-xValues=[];
-for (var i = 0; i < bs_temp.length; i++) {
-  xValues.push(i+1);
-}
-
-// Trace for the chart
-var trace = {
-  x: xValues,
-  y: bs_temp,
-  type: 'scatter',
-  mode: 'lines+markers', 
-  marker: {
-    color: 'white' 
-  },
-  line: {
-    color: 'lightgreen' // Line color
+  // Data for the chart
+  xValues = [];
+  for (var i = 0; i < bs_temp.length; i++) {
+    xValues.push(i + 1);
   }
-};
 
-// Data array
-var data = [trace];
+  // Trace for the chart
+  var trace = {
+    x: xValues,
+    y: bs_temp,
+    type: 'scatter',
+    mode: 'lines+markers',
+    marker: {
+      color: 'white'
+    },
+    line: {
+      color: 'lightgreen' // Line color
+    }
+  };
 
-var layout = {
-  plot_bgcolor: 'black',    
-  paper_bgcolor: 'black',
-  xaxis: {
-    color: 'white',
-    title:'Time' // X-axis text color
-  },
-  yaxis: {
-    color: 'white', // Y-axis text color
-    title:'Temperature',
-  }
-  
-};
+  // Data array
+  var data = [trace];
+
+  var layout = {
+    plot_bgcolor: 'black',
+    paper_bgcolor: 'black',
+    xaxis: {
+      color: 'white',
+      title: 'Time' // X-axis text color
+    },
+    yaxis: {
+      color: 'white', // Y-axis text color
+      title: 'Temperature',
+    }
+
+  };
 
 
 
-// Plot the chart
-Plotly.newPlot('temp', data, layout);
+  // Plot the chart
+  Plotly.newPlot('temp', data, layout);
 
 }
 
@@ -47,5 +47,5 @@ Plotly.newPlot('temp', data, layout);
 
 var bs_temp = [31.7, 31.7, 31.7, 31.7, 31.7, 31.7, 31.7, 31.7, 31.7, 31.8, 31.8, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 32, 32, 32.1, 32.1, 32.1, 32.2, 32.2, 31.3, 31.8, 31.4, 31.2, 32.6, 32, 32, 32, 32, 32, 32.1, 32, 32, 32, 32, 32.1, 32.1, 32, 32, 32, 32, 32, 32, 32, 32, 32.1, 32, 32.1, 32, 32, 32, 32.1, 32, 32, 32, 32, 32, 31.9, 32, 32, 32, 32, 32, 32.1, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 31.9, 32, 31.9, 32, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 31.9, 32, 32, 32.1, 32, 31.9, 32, 32, 32, 32, 32, 31.9, 32, 32.1, 32, 32, 32, 32, 32, 32.1, 32, 32, 32, 32, 32, 31.9, 32, 31.9, 32.1, 31.9, 32, 31.9, 32.1, 32, 32, 32, 32, 32.1, 32, 32, 31.9, 32, 32, 31.9, 32, 32, 32, 31.9, 32, 32, 32, 32, 32, 32, 32.1, 32, 32.1, 32, 32, 32.1, 32.1, 32.1, 32, 32, 32, 32, 32, 32.1, 32.1, 32, 32, 32.1, 32, 32.2, 32.1, 32.1, 32, 32, 32.2, 32.1, 32, 32, 32.1, 32.1, 32.1, 32.1, 32.2, 32.1, 32.1, 32.2];
 
-plot3( bs_temp);
+plot3(bs_temp);
 
