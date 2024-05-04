eec3b32b9e674dbb93963a51f632a1482484d08e
Bhushan Songire <bhushanbsongire@gmail.com>
2024-05-04 23:10:07 +0530

    update time

--- qualityReadings.js
+++ qualityReadings.js
@@ -1,3 +1,5 @@
+const last_updated = document.getElementById("last_updated");
+
 function updateReadings(temp, hum, aq) {
   const dash_temp = document.getElementById('dash_temp');
   const dash_aq = document.getElementById('dash_aq');
@@ -6,6 +8,14 @@ function updateReadings(temp, hum, aq) {
   dash_temp.innerHTML = `${temp} °C`;
   dash_hum.innerHTML = `${hum} %`;
   dash_aq.innerHTML = `${aq} `;
+
+  var now = new Date(Date.now());
+  var hours = String(now.getHours()).padStart(2, '0');
+  var minutes = String(now.getMinutes()).padStart(2, '0');
+  var seconds = String(now.getSeconds()).padStart(2, '0');
+  // console.log(`Last Updated: ${hours}:${minutes}:${seconds}`);
+  last_updated.innerHTML = `Last Updated: ${hours}:${minutes}:${seconds}`;
+
   return true;
 }
 
