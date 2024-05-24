4dd8d84539a7dfe62ac9a9b3cf42b7436bebc37d
Bhushan Songire <bhushanbsongire@gmail.com>
2024-05-24 12:21:57 +0530

    exp3

--- fb_get.js
+++ fb_get.js
@@ -1,7 +1,7 @@
 // this will run on the client side:
 
 export const getConfig = async () => {
-    const response = await fetch('/api/config');
+    const response = await fetch('/api/fb_env.js');
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
