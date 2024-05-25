e342894bd3a412ad51a2f81690f8a5e7a3d2ab5c
Bhushan Songire <bhushanbsongire@gmail.com>
2024-05-26 00:21:00 +0530

    added alert icon support

--- trigger_error.js
+++ trigger_error.js
@@ -10,23 +10,29 @@ function activate_error_box(fire, gas) {
         err_box.style.display = 'flex';
 
         if (fire && gas) {
-            warning_title.innerHTML = "Fire and Harmful Gases ";
+            warning_title_text.innerHTML = "Fire and <br> Harmful Gases ";
 
-            warning_detail.innerHTML = "Fire and Harmful gases is Detected in your house Evacuate!!";
+            err_icon.src = "./assets/i_emergency.png";
+
+            warning_detail.innerHTML = "Fire and high level of Harmful gases have been detected in your house. <br> Evacuate immediately !!!";
 
             showNotification("Emergency Alert!", "Elevated levels of harmful gases and fire have been detected in your residence. Please evacuate immediately for your safety!”");
         }
         else if (fire) {
-            warning_title.innerHTML = "Fire🔥";
+            warning_title_text.innerHTML = "Flames <br> Detected";
 
-            warning_detail.innerHTML = "Fire Detected in your house   Evacuate!!";
+            err_icon.src = "./assets/i_burn.png";
+
+            warning_detail.innerHTML = "Fire Detected in your house. <br> Evacuate immediately !!!";
 
             showNotification("Fire Emergency!", "Fire is detected at your residence!! Evacuate immediately for your safety ");
         }
         else if (gas) {
-            warning_title.innerHTML = "Gas";
+            warning_title_text.innerHTML = "Gas Leakage <br> Detected";
 
-            warning_detail.innerHTML = "Harmful Gases are detected. Evacuate immedately !!";
+            err_icon.src = "./assets/i_gas_leak.png";
+
+            warning_detail.innerHTML = "Raised level of Harmful gases has been detected in your house. <br> Evacuate immediately !!!";
 
             showNotification("Gas Emergency!", "Harmful Gases are detected !!  Evacuate immediately ");
         }
@@ -35,12 +41,13 @@ function activate_error_box(fire, gas) {
     else {
         mn.style.cssText = 'filter: none';
         err_box.style.display = 'none'
-        warning_title.innerHTML = "";
+        warning_title_text.innerHTML = "";
         warning_detail.innerHTML = "";
     }
 
     return true;
 }
+
 
 // <!-- Notification script: -->
 // Request permission to show notifications when the page is loaded
